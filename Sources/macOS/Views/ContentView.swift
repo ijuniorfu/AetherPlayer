f03a2e28f5c93162a26d310dca54ec900961ae6c
junior <ijuniorfu@gmail.com>
2026-07-18 13:41:35 +0800

    Update

--- Sources/macOS/Views/ContentView.swift
+++ Sources/macOS/Views/ContentView.swift
@@ -78,7 +78,7 @@ struct ContentView: View {
                         // a external track; anything else loads as a new video.
                         model.loadExternalSubtitle(url: url)
                     } else {
-                        await model.open(url: URL(string: "http://127.0.0.1:9976/proxy.iso?url=https%3A%2F%2F223-109-125-232-v3.pd1.cjjd19.com%2F1135-vip-download-cdn.123295.com%2F123-332%2F7da1cbc1%2F1850695235-0%2F7da1cbc117f8193256c41efca255994c%2Fc-m101%3Fv%3D5%26t%3D1784954769%26r%3DQS4BDA%26bzc%3D1%26bzs%3D313832373731343731333a323a313a3334393630323434373336%26ur%3Dvplgavagpigbl%26urn%3D1%26s%3D1784954769eb5bef710f8fda9f69f82d42254ad0ed%26bzp%3D0%26bi%3D876316113%26filename%3D%E5%94%AC%E8%83%86%E7%89%B9%E5%B7%A5%2B%255B%E7%BE%8E%E7%89%88%E5%8E%9F%E7%9B%98%2B%E5%8E%9F%E7%94%9F%E4%B8%AD%E5%AD%97%2BDIY%E4%B8%8A%E8%AF%91%E5%85%AC%E6%98%A0%E5%9B%BD%E8%AF%AD%2B%E5%9B%BD%E9%85%8D%E7%AE%80%E4%BD%93%E5%AD%97%E5%B9%95%255D.The%2BMan%2Bfrom%2BToronto%2B2022%2BBluRay%2B1080p%2BAVC%2BDTS-HD%2BMA5.1-TYZH%2540HDSky%255B32.56GB%255D%25281%2529.iso%26x-mf-biz-cid%3D04610814-9aae-4ac0-8b1d-b656c950cc88-5baabb%26auto_redirect%3D0%26ndcp%3D1%26cache_type%3D1")!)
+                        await model.open(url: url)
                     }
                 }
             }
